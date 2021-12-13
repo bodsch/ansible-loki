c2561f47f6df1e315a8d619e85e253f3d511362e
Bodo Schulz <bodo.schulz@tallence.com>
2021-12-13 16:02:23 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,50 @@
+
+# Ansible Role:  `loki`
+
+Ansible role to setup [Loki](https://github.com/grafana/loki).
+
+
+[![GitHub Workflow Status](https://img.shields.io/github/workflow/status/bodsch/ansible-loki/CI)][ci]
+[![GitHub issues](https://img.shields.io/github/issues/bodsch/ansible-loki)][issues]
+[![GitHub release (latest by date)](https://img.shields.io/github/v/release/bodsch/ansible-loki)][releases]
+
+[ci]: https://github.com/bodsch/ansible-loki/actions
+[issues]: https://github.com/bodsch/ansible-loki/issues?q=is%3Aopen+is%3Aissue
+[releases]: https://github.com/bodsch/ansible-loki/releases
+
+
+## Requirements & Dependencies
+
+- None
+
+### Operating systems
+
+Tested on
+
+* Debian 10
+* Ubuntu 20.10
+* CentOS 8
+* Oracle Linux 8
+* Arch Linux
+
+## Contribution
+
+Please read [Contribution](CONTRIBUTING.md)
+
+## Development,  Branches (Git Tags)
+
+The `master` Branch is my *Working Horse* includes the "latest, hot shit" and can be complete broken!
+
+If you want to use something stable, please use a [Tagged Version](https://gitlab.com/bodsch/ansible-icinga2/-/tags)!
+
+---
+
+## Author and License
+
+- Bodo Schulz
+
+## License
+
+[Apache](LICENSE)
+
+**FREE SOFTWARE, HELL YEAH!**
---
+++ molecule/2.4/tests/test_default.py
@@ -0,0 +1,150 @@
+# coding: utf-8
+from __future__ import unicode_literals
+
+from ansible.parsing.dataloader import DataLoader
+from ansible.template import Templar
+
+import json
+import pytest
+import os
+
+import testinfra.utils.ansible_runner
+
+HOST = 'instance'
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts(HOST)
+
+
+def pp_json(json_thing, sort=True, indents=2):
+    if type(json_thing) is str:
+        print(json.dumps(json.loads(json_thing), sort_keys=sort, indent=indents))
+    else:
+        print(json.dumps(json_thing, sort_keys=sort, indent=indents))
+    return None
+
+
+def base_directory():
+    """ ... """
+    cwd = os.getcwd()
+
+    if('group_vars' in os.listdir(cwd)):
+        directory = "../.."
+        molecule_directory = "."
+    else:
+        directory = "."
+        molecule_directory = "molecule/{}".format(os.environ.get('MOLECULE_SCENARIO_NAME'))
+
+    return directory, molecule_directory
+
+
+def read_ansible_yaml(file_name, role_name):
+    ext_arr = ["yml", "yaml"]
+
+    read_file = None
+
+    for e in ext_arr:
+        test_file = "{}.{}".format(file_name, e)
+        if os.path.isfile(test_file):
+            read_file = test_file
+            break
+
+    return "file={} name={}".format(read_file, role_name)
+
+
+@pytest.fixture()
+def get_vars(host):
+    """
+        parse ansible variables
+        - defaults/main.yml
+        - vars/main.yml
+        - vars/${DISTRIBUTION}.yaml
+        - molecule/${MOLECULE_SCENARIO_NAME}/group_vars/all/vars.yml
+    """
+    base_dir, molecule_dir = base_directory()
+    distribution = host.system_info.distribution
+
+    if distribution in ['debian', 'ubuntu']:
+        os = "debian"
+    elif distribution in ['centos', 'redhat', 'ol']:
+        os = "redhat"
+    elif distribution in ['arch']:
+        os = "archlinux"
+
+    print(" -> {} / {}".format(distribution, os))
+
+    file_defaults      = read_ansible_yaml("{}/defaults/main".format(base_dir), "role_defaults")
+    file_vars          = read_ansible_yaml("{}/vars/main".format(base_dir), "role_vars")
+    file_distibution   = read_ansible_yaml("{}/vars/{}".format(base_dir, os), "role_distibution")
+    file_molecule      = read_ansible_yaml("{}/group_vars/all/vars".format(base_dir), "test_vars")
+    # file_host_molecule = read_ansible_yaml("{}/host_vars/{}/vars".format(base_dir, HOST), "host_vars")
+
+    defaults_vars      = host.ansible("include_vars", file_defaults).get("ansible_facts").get("role_defaults")
+    vars_vars          = host.ansible("include_vars", file_vars).get("ansible_facts").get("role_vars")
+    distibution_vars   = host.ansible("include_vars", file_distibution).get("ansible_facts").get("role_distibution")
+    molecule_vars      = host.ansible("include_vars", file_molecule).get("ansible_facts").get("test_vars")
+    # host_vars          = host.ansible("include_vars", file_host_molecule).get("ansible_facts").get("host_vars")
+
+    ansible_vars = defaults_vars
+    ansible_vars.update(vars_vars)
+    ansible_vars.update(distibution_vars)
+    ansible_vars.update(molecule_vars)
+    # ansible_vars.update(host_vars)
+
+    templar = Templar(loader=DataLoader(), variables=ansible_vars)
+    result = templar.template(ansible_vars, fail_on_undefined=False)
+
+    return result
+
+
+def test_package(host, get_vars):
+    packages = get_vars.get("redis_packages")
+
+    for pack in packages:
+        p = host.package(pack)
+        assert p.is_installed
+
+
+@pytest.mark.parametrize("dirs", [
+    "/etc/redis.d",
+])
+def test_directories(host, dirs):
+    d = host.file(dirs)
+    assert d.is_directory
+    assert d.exists
+
+
+@pytest.mark.parametrize("files", [
+    "/etc/redis.d/general.conf"
+])
+def test_files(host, files):
+    f = host.file(files)
+    assert f.exists
+    assert f.is_file
+
+
+def test_user(host):
+    assert host.group("redis").exists
+    assert host.user("redis").exists
+    assert "redis" in host.user("redis").groups
+    # assert host.user("redis").shell == "/sbin/nologin"
+    assert host.user("redis").home == "/var/lib/redis"
+
+
+def test_service(host, get_vars):
+    service_name = get_vars.get("redis_daemon")
+
+    service = host.service(service_name)
+    assert service.is_enabled
+    assert service.is_running
+
+
+def test_open_port(host, get_vars):
+    for i in host.socket.get_listening_sockets():
+        print(i)
+
+    bind_address = get_vars.get("redis_network_bind")
+    bind_port = get_vars.get("redis_network_port")
+
+    service = host.socket("tcp://{0}:{1}".format(bind_address, bind_port))
+    assert service.is_listening
